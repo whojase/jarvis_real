45c2465169837694c248ec16a4f61ae756580931
whojase <vvvdddd503@gmail.com>
2024-11-30 17:16:00 +0300

    Add files via upload

---
+++ app/play_sound.py
@@ -0,0 +1,8 @@
+import pygame
+
+def play_sound(file_path):
+    pygame.mixer.init()
+    pygame.mixer.music.load(file_path)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        continue
--- main.py
+++ main.py
@@ -4,86 +4,79 @@ import datetime
 import time
 import webbrowser
 import os
+import pyautogui
+from playsound import playsound
 
 from comands_jarvis import com
+from app.play_sound import play_sound
 
+# Настройки речевого движка
+def speak(text):
+    """Озвучивает текст с использованием pyttsx3."""
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+
+# Настройки микрофона
 r = sr.Recognizer()
 m = sr.Microphone()
 
 sr.LANGUAGE = 'ru-RU'
 
-engine = pyttsx3.init()
-
-print("Запись пошла, говорите. Чтобы завершить, скажите 'стоп'.")
 
 while True:
     with m as source:
         r.adjust_for_ambient_noise(source)
         print("Слушаю... Говорите!")
-
         audio = r.listen(source, timeout=None, phrase_time_limit=None)
         
     try:
         text = r.recognize_google(audio, language='ru-RU')
         print(f'Вы сказали: {text.lower()}')
 
+        # Реакция на команды
         if text.lower() in com['alies']:
-            engine.say("Привет, как я могу помочь?")
-            engine.runAndWait()
-
-        elif text.lower() in com['time']:
-            now = datetime.datetime.now()
-            current_time = now.strftime("%H:%M")
-            engine.say('Выполняю сэр.')
-            time.sleep(1)
-            engine.say(f"Текущее время: {current_time}")
-            engine.runAndWait()
+            play_sound('sounds\hello.wav')
 
         elif text.lower() in com['bye']:
-            engine.say("До свидания!")
-            engine.runAndWait()
             break
 
-        elif text.lower() in com['help']:
-            engine.say(f'Я голосовой помощник со встроеным искуственым интелектом разработаный тони страком. У меня есть множество функций, от обычного открывания програм - до диогностики системы')
-            engine.runAndWait()
-        
         elif text.lower() in com['open_browser']:
-            engine.say('Секунду, выполняю сэр')
-            time.sleep(1)
-            engine.say("Открываю браузер.")
-            engine.runAndWait()
             webbrowser.open('https://www.google.com')
-        
+            play_sound('sounds\comfirm.wav')
+
         elif text.lower() in com['open_explorer']:
-            engine.say('Выполняю, сэр')
-            time.sleep(1)
-            engine.say("Открываю проводник.")
-            engine.runAndWait()
             os.system('explorer')
-        
+            play_sound('sounds/always_at_your_service.wav')
+
         elif text.lower() in com['open_youtube']:
-            engine.say('Секунду, выполняю сэр')
-            time.sleep(1)
-            engine.say("Открываю ютуб.")
-            engine.runAndWait()
+            play_sound('sounds/loading.wav')
             webbrowser.open('https://www.youtube.com')
-        
+
         elif text.lower() in com['open_vk']:
-            engine.say('Секунду, выполняю сэр')
-            time.sleep(1)
-            engine.say("Открываю вконтакте.")
-            engine.runAndWait()
+            play_sound('sounds/no_problem.wav')
             webbrowser.open('https://www.vk.ru')
 
         elif text.lower() in com['open_funpay']:
-            engine.say('Секунду, выполняю сэр')
-            time.sleep(1)
-            engine.say("Открываю funpay.")
-            engine.runAndWait()
+            play_sound('sounds/always.wav')
             webbrowser.open('https://www.funpay.com')
 
+        elif text.lower() in com['shutdown']:
+            play_sound('sounds/off.wav')
+            os.system('shutdown /s /t 1')
+        
+
+        else:
+            # Ответ на неизвестную команду
+            response = "Извините, я не понял вашу команду."
+            speak(response)
+
     except sr.UnknownValueError:
-        print("Не удалось распознать речь, повторите.")
+        response = "Не удалось распознать речь, повторите."
+        print(response)
+        speak(response)
+
     except sr.RequestError:
-        print("Ошибка соединения с сервисом Google. Попробуйте позже.")
+        response = "Ошибка соединения с сервисом Google. Попробуйте позже."
+        print(response)
+        speak(response)
